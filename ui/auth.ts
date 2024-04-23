4324b29bb7775c3fae270ed00448a25497aab5f4
aimerzarashi <git.aimerzarashi.com>
2024-04-23 21:55:12 +0900

    auth.js Nodemailer Provider setup is complete

--- ui/auth.ts
+++ ui/auth.ts
@@ -1,5 +1,16 @@
 import NextAuth from "next-auth"
+import Nodemailer from "next-auth/providers/nodemailer"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Nodemailer({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
 })
